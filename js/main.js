3e09ae8dd7d29d5dfccb291f4399d38d9580387e
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-09-19 02:18:16 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 const toRules = document.querySelectorAll('.toRules')
 const toRegister = document.querySelectorAll('.toRegister')
+
+// тригер на открытие попапа "Спасибо" 
+const toThanks = document.querySelectorAll('.toThanks')
+
 const rulesModal = document.querySelector('#rulesModal')
 const registerModal = document.querySelector('#registerModal')
+
+// попап "Спасибо"
+const thanksModal = document.querySelector('#thanksModal')
+
+
+
 const closeModal = document.querySelectorAll('.modal-close')
+
+// Закрытие попапа "Спасибо"
+const closeThanks = thanksModal.querySelector('.close')
 
 if (closeModal)
   closeModal.forEach(item => {
@@ -30,4 +43,22 @@ if (toRegister && registerModal)
       registerModal.classList.add('active')
       document.querySelector('body').style.overflowY = 'hidden'
     })
+  })
+
+
+// открытие попапа "Спасибо"
+if (toThanks && thanksModal)
+  toThanks.forEach(item => {
+    item.addEventListener('click', function () {
+      thanksModal.classList.add('active')
+      document.querySelector('body').style.overflowY = 'hidden'
+    })
+  })
+
+if (thanksModal && closeThanks)
+  closeThanks.addEventListener('click', function () {
+    thanksModal.classList.remove('active')
+
+    document.querySelector('body').style.overflowY = 'auto'
+
   })
