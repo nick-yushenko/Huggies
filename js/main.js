e784e2753fd951b341a53216ef7462a4820589c1
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-09-22 10:30:59 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -62,3 +62,84 @@ if (thanksModal && closeThanks)
     document.querySelector('body').style.overflowY = 'auto'
 
   })
+
+//  Слайдеры
+
+var programSlider = new Swiper('.program-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  speed: 500,
+  spaceBetween: 50,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.program-slider__next',
+    prevEl: '.program-slider__prev',
+  },
+
+
+})
+const programSlides = document.querySelectorAll('.program-slide')
+const programPrev = document.querySelector('.program-slider__prev')
+const programNext = document.querySelector('.program-slider__next')
+
+programSlider.on('slideChange', function () {
+  if (programSlider.activeIndex == 0)
+    programPrev.classList.remove('clickable')
+  else
+    programPrev.classList.add('clickable')
+
+  if (programSlider.activeIndex == (programSlides.length - 1))
+    programNext.classList.remove('clickable')
+  else
+    programNext.classList.add('clickable')
+});
+
+
+function setBoostSlider(){
+  var boostSlider = new Swiper('.boost-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 300,
+    spaceBetween: 30,
+    slidesPerView: 3,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.boost-slider__next',
+      prevEl: '.boost-slider__prev',
+    },
+  
+  
+  })
+}
+
+if (window.innerWidth <= 1024)
+  setBoostSlider()
+// чекбоксы для форм 
+
+// const checkboxes = document.querySelectorAll('.checkbox')
+
+// if (checkboxes)
+//   // checkboxes.forEach(item => {
+//   //   if (item.querySelector('input').getAttribute('name') == 'brif')
+//   //     console.log(item)
+//   // })
+//   checkboxes.forEach(item => {
+//     const input = item.querySelector('input')
+//     if (input.hasAttribute('checked'))
+//       item.classList.add('checked')
+//     else
+//       item.classList.remove('checked')
+
+//     input.onchange = function () {
+//       if (input.hasAttribute('checked'))
+//         item.classList.remove('checked')
+//       else
+//         item.classList.add('checked')
+
+//       if (item.querySelector('input').getAttribute('name') == 'brif' && )
+//       //     console.log(item)
+
+//     }
+//   })
